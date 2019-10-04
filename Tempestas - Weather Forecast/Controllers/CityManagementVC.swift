213af0b92036adb59d4e6895ea394a88f401c615
Nikolaos Rafail Nikolouzos <nickrnikolouzos@gmail.com>
2019-10-04 20:18:55 +0300

    Started adding functionality to the CityManagementVC

--- Tempestas - Weather Forecast/Controllers/CityManagementVC.swift
+++ Tempestas - Weather Forecast/Controllers/CityManagementVC.swift
@@ -10,9 +10,35 @@ import UIKit
 
 /// This ViewController manages the user's saved cities and performs weather forecast searches
 class CityManagementVC: UIViewController {
-
+    
+    @IBOutlet weak var citySearchField: CustomAnimatableTextField!
+    
+    @IBOutlet weak var savedCitiesTableView: UITableView!
+    var cities: Cities = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Remove the tableview's footer
+        savedCitiesTableView.tableFooterView = UIView()
+    }
+}
+
+// MARK: - TableView Delegate & Data Source implementation
+extension CityManagementVC: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Create the CityCell
+        guard let cityCell = tableView.dequeueReusableCell(
+            withIdentifier: "cityCell") as? CityCell else { return UITableViewCell() }
+        
+        // Customize the city cell
+        let currentCity = cities[indexPath.item]
+        cityCell.setupCell(forCity: currentCity)
+        
+        return cityCell
     }
 }

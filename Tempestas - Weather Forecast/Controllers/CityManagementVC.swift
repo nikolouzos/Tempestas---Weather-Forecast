c1a6d08fa35dde6ef1cd389739a939906a71718c
Nikolaos Rafail Nikolouzos <nick@nikolouzos.xyz>
2019-10-05 01:22:21 +0300

    Added the add and remove from the saved cities functionality - Minor layout changes in Main.storyboard

--- Tempestas - Weather Forecast/Controllers/CityManagementVC.swift
+++ Tempestas - Weather Forecast/Controllers/CityManagementVC.swift
@@ -14,6 +14,7 @@ class CityManagementVC: UIViewController {
     @IBOutlet weak var citySearchField: CustomAnimatableTextField!
     
     @IBOutlet weak var savedCitiesTitle: UILabel!
+    @IBOutlet weak var savedCitiesDescription: UILabel!
     @IBOutlet weak var savedCitiesTableView: UITableView!
     
     var citiesWrapper = CitiesWrapper(withKey: "savedCities")
@@ -26,6 +27,64 @@ class CityManagementVC: UIViewController {
         
         // Load the CityCell XIB
         savedCitiesTableView.register(UINib(nibName: "CityCell", bundle: nil), forCellReuseIdentifier: "cityCell")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Toggle the saved cities texts
+        toggleSavedCitiesTexts()
+    }
+    
+    /// Shows or hides the saved cities title and description
+    func toggleSavedCitiesTexts() {
+        savedCitiesTitle.isHidden = citiesWrapper.cities.isEmpty
+        savedCitiesDescription.isHidden = citiesWrapper.cities.isEmpty
+        
+        // Animate the changes
+        UIView.animate(withDuration: 0.25, animations: {
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    /// Requests the weather forecast from the API
+    func requestWeatherForecast() {
+        // Make sure there's an input and format it correctly
+        if let city = citySearchField.text?.replacingOccurrences(of: " ", with: "+"), !city.isEmpty {
+            // Start a new weather forecast request
+            Networking.shared.getWeatherForecast(forCity: city)
+            
+            // Reset the citySearchField
+            citySearchField.text = ""
+        }
+        
+        // Dismiss the keyboard
+        citySearchField.resignFirstResponder()
+    }
+    
+    /// Saves the textField's input to the cities array, updates the tableView and clears the textField
+    @IBAction func onSaveButtonTap(_ sender: Any) {
+        guard let city = citySearchField.text, !city.isEmpty else { return }
+        
+        // Make sure the inserted city is not in the array
+        if !citiesWrapper.cities.contains(city) {
+            
+            // Add the new element to the cities
+            citiesWrapper.cities.insert(city, at: 0)
+            
+            // Save the new array to the UserDefaults
+            citiesWrapper.save()
+            
+            // Update the saved cities texts
+            toggleSavedCitiesTexts()
+            
+            // Update the savedCitiesTableView
+            savedCitiesTableView.insertRows(at: [IndexPath(item: 0, section: 0)], with: .fade)
+        }
+        
+        // Clear the textField and hide the keyboard
+        citySearchField.text = ""
+        citySearchField.resignFirstResponder()
     }
 }
 
@@ -48,7 +107,26 @@ extension CityManagementVC: UITableViewDataSource, UITableViewDelegate {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-       
+        // Request the weather forecast
+        requestWeatherForecast()
+    }
+    
+    /// Adds the Swipe to Delete functionality to the tableView
+    func tableView(_ tableView: UITableView,
+                   commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Remove the city from the cities
+            citiesWrapper.cities.remove(at: indexPath.item)
+            
+            // Update the UserDefaults
+            citiesWrapper.save()
+            
+            // Update the tableView
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+            // Update the saved cities texts
+            toggleSavedCitiesTexts()
+        }
     }
 }
 
@@ -57,17 +135,8 @@ extension CityManagementVC: UITextFieldDelegate {
     
     /// Starts a request depending on the content of the textField & dismisses the keyboard
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // Make sure there's an input and format it correctly
-        if let city = textField.text?.replacingOccurrences(of: " ", with: "+"), !city.isEmpty {
-            // Start a new weather forecast request
-            Networking.shared.getWeatherForecast(forCity: city)
-            
-            // Reset the textField
-            textField.text = ""
-        }
-        
-        // Dismiss the keyboard
-        textField.resignFirstResponder()
+        // Request the weather forecast
+        requestWeatherForecast()
         
         return true
     }

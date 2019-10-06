6ff3769174e8e3976ad8dfed171fd8b3aef2e167
Nikolaos Rafail Nikolouzos <nick@nikolouzos.xyz>
2019-10-06 21:12:40 +0300

    Added the forecast day CollectionView functionality - Minor layout fixes - Added the ForecastDayCell functionality

--- Tempestas - Weather Forecast/Views/ForecastDayCell.swift
+++ Tempestas - Weather Forecast/Views/ForecastDayCell.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import IBAnimatable
 
 class ForecastDayCell: UICollectionViewCell {
+    @IBOutlet weak var forecastDayLabel: UILabel!
+    @IBOutlet weak var activeLabel: AnimatableLabel!
     
+    /// Sets up the cell with the data supplied
+    func setupCell(for day: Int, isSelected: Bool) {
+        
+        // Create and set the day on the forecastDayLabel
+        forecastDayLabel.text = getDayString(for: day)
+        
+        // Fade the activeLabel in or out. This updates only if needed
+        activeLabel.animate(.fade(way: isSelected ? .in : .out))
+    }
+    
+    /// Gets the day string for the forecastDayLabel
+    private func getDayString(for day: Int) -> String {
+        // Try to return a custom day
+        switch day {
+        case 0: return "Today"
+        case 1: return "Tomorrow"
+        default: break
+        }
+        
+        // If the day is not Today or Tomorrow format the date using a DateFormatter
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEE d MMM"
+        dateFormatter.locale = Locale.current
+        
+        // Create the date for the cell day
+        let date = Date().addingTimeInterval(TimeInterval(86400 * day))
+        
+        return dateFormatter.string(from: date)
+    }
 }

ed646d2625c5fca3ecd843706ef633284532db64
Nikolaos Rafail Nikolouzos <nick@nikolouzos.xyz>
2019-10-06 18:26:51 +0300

    Added the ForecastDayCell class

---
+++ Tempestas - Weather Forecast/Views/ForecastDayCell.swift
@@ -0,0 +1,13 @@
+//
+//  ForecastDayCell.swift
+//  Tempestas - Weather Forecast
+//
+//  Created by Nikolaos Rafail Nikolouzos on 6/10/19.
+//  Copyright © 2019 Nikolaos Rafail Nikolouzos. All rights reserved.
+//
+
+import UIKit
+
+class ForecastDayCell: UICollectionViewCell {
+    
+}
